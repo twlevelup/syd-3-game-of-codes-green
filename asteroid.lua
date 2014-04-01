275504b25f0941d4c9fb2d68bdb1d9b638357d1c
Jarrod Jeffrey Ingram <jing0012@uni.sydney.edu.au>
2014-04-01 12:25:11 +1100

    Asteroids now travel from one place to another using a speed and angle, the image is also rotated based on this angle

--- asteroid.lua
+++ asteroid.lua
@@ -10,43 +10,42 @@ function Asteroid:new(game, config)
 
   math.randomseed(os.time())
   newAsteroid.x = config.x or 800
-  newAsteroid.y = config.y or math.random(100, 500)
-  newAsteroid.speed = config.speed or {
-    x = 250,
-    y = math.random(-100, 100)
-  }
-  newAsteroid.size = config.size or {
-    x = 100,
-    y = 100
-  }
-  newAsteroid.graphics = config.graphics or {
-    source = "assets/images/meteor.png"
-  }
+  newAsteroid.y = config.y or math.random(0, 500)
+  newAsteroid.to = config.to or {x = 0, y = math.random(0, 500)}
 
+  newAsteroid.angle = math.atan2(newAsteroid.y - newAsteroid.to.y, newAsteroid.x - newAsteroid.to.x)
+  newAsteroid.speed = config.speed or 300
+  newAsteroid.dx = newAsteroid.speed * math.cos(newAsteroid.angle)
+  newAsteroid.dy = newAsteroid.speed * math.sin(newAsteroid.angle)
 
+  newAsteroid.size = config.size or {x = 100, y = 100}
+
+  newAsteroid.graphics = config.graphics or {source = "assets/images/meteor.png"}
   if game.graphics ~= nil and game.animation ~= nil then
     newAsteroid.graphics.sprites = game.graphics.newImage(newAsteroid.graphics.source)
-    newAsteroid.graphics.grid = game.animation.newGrid(
-      newAsteroid.size.x, newAsteroid.size.y,
-      newAsteroid.graphics.sprites:getWidth(),
-      newAsteroid.graphics.sprites:getHeight()
-    )
-    newAsteroid.graphics.animation = game.animation.newAnimation(
-      newAsteroid.graphics.grid("1-1", 1),
-      0.05
-    )
     newAsteroid.sx = 100 / newAsteroid.graphics.sprites:getWidth()
     newAsteroid.sy = 100 / newAsteroid.graphics.sprites:getHeight()
+    -- No animations for now.
+    --
+    -- newAsteroid.graphics.grid = game.animation.newGrid(
+    --   newAsteroid.size.x, newAsteroid.size.y,
+    --   newAsteroid.graphics.sprites:getWidth(),
+    --   newAsteroid.graphics.sprites:getHeight()
+    -- )
+    -- newAsteroid.graphics.animation = game.animation.newAnimation(
+    --   newAsteroid.graphics.grid("1-1", 1),
+    --   0.05
+    -- )
   end
 
   return setmetatable(newAsteroid, self)
 end
 
 function Asteroid:update(dt)
-  self.x = self.x - self.speed.x * dt
-  self.y = self.y - self.speed.y * dt
+  self.x = self.x - self.dx * dt
+  self.y = self.y - self.dy * dt
 end
 
 function Asteroid:draw()
-  self.game.graphics.draw(self.graphics.sprites, self.x, self.y, 0, self.sx, self.sy)
+  self.game.graphics.draw(self.graphics.sprites, self.x, self.y, self.angle, self.sx, self.sy)
 end
