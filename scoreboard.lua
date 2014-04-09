1fdf1759171d0ffa39d112d96a402be7e8df5e45
Gumibear <gamieger@gmail.com>
2014-04-09 10:28:19 +1000

    Add exit game function

--- scoreboard.lua
+++ scoreboard.lua
@@ -9,6 +9,7 @@ function Scoreboard:enter()
   Scoreboard.y = love.window.getHeight() * 0.1 
   Scoreboard.width = love.window.getWidth() * 0.6
   Scoreboard.height = love.window.getHeight() * 0.8
+  Scoreboard.message = "Game Over" .. "\nPress \"Space\" to start again" .. "\n Press \"Esc\" to exit the game"
 end
 
 function Scoreboard:update(dt)
@@ -19,11 +20,13 @@ function Scoreboard:draw()
   love.graphics.setColor(56,57,59)
   love.graphics.rectangle("fill", Scoreboard.x, Scoreboard.y, Scoreboard.width, Scoreboard.height)
   love.graphics.setColor(255,255,255)
-  love.graphics.printf("Game Over, \nPress \"Space\" to start again", love.window.getWidth() * 0.25, Scoreboard.y + HEADING_OFFSET, LINE_LIMIT, "center", 0, 1, 1.5)  
+  love.graphics.printf(Scoreboard.message, love.window.getWidth() * 0.25, Scoreboard.y + HEADING_OFFSET, LINE_LIMIT, "center", 0, 1, 1.5)  
 end
 
 function Scoreboard:keyreleased(key)
     if key == ' ' then
       love.state.switch(game)
+    elseif key == "escape" then
+      love.event.quit()
     end
 end
