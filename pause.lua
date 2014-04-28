eebf139130e8a0d0c5d4891bbceafb2d89acbe8d
Harrison Conlin <me@harrisony.com>
2014-04-28 18:46:37 +1000

    Pause instructions no longer shown when game paused

--- pause.lua
+++ pause.lua
@@ -13,7 +13,7 @@ function Pause:draw()
     -- draw previous screen
     self.from:draw()
     -- overlay with pause message
-    love.graphics.setColor(0,0,0, 100)
+    love.graphics.setColor(0,0,0, 255)
     love.graphics.rectangle('fill', 0, 0, W, H)
     love.graphics.setColor(255, 255, 255)
     love.graphics.printf('PAUSED', 0, H/2, W, 'center')
